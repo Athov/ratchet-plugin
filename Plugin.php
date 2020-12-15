4382f5825250f912011a6dd0a55b588570274653
Atanas Harapov <8526043+Athov@users.noreply.github.com>
2020-12-15 09:42:39 +0200

    Fix ratchet provider not registering
    Fix ratchet provider not registering after installing from OctoberCMS store

--- Plugin.php
+++ Plugin.php
@@ -2,12 +2,15 @@
 
 use System\Classes\PluginBase;
 use Config;
+use App;
+use Askedio\LaravelRatchet\Providers\LaravelRatchetServiceProvider;
 
 class Plugin extends PluginBase
 {
     public function boot()
     {
         $this->setConfiguration();
+        App::register(LaravelRatchetServiceProvider::class);
     }
 
     public function setConfiguration()
